e4fd1b17e88c1eae3255bda358f856b9811a1ac6
Shantanu <shantanujain@hotmail.co.uk>
2018-02-03 18:54:23 +0000

    Editing prac_1

--- prac_1.cpp
+++ prac_1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
 using namespace std;
 
+class A {
+public:
+	A();
+	A(int x){ a = x; }
+	display() {
+		cout << a ;
+	}
+private:
+	int a;
+};
+
 int main() {
 	cout << "Hello World \n";
+	A obj1(4);
+	obj1.display();
+	//cout << obj1.a << "\n";
 	return 0;
 }
